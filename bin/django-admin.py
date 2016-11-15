e9375174bb516b2bf7e59a318050c070f5e7aa33
blaru001 <blaru001@fiu.edu>
2016-11-15 16:51:40 -0500

    Remove the now ignored directory lib bin and include

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/nikon/Documents/projects/advising_plus/advising_plus/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
