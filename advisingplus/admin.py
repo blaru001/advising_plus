a7df8732b2d21e702ab8474d3d4c2d85a4299838
blaru001 <blaru001@fiu.edu>
2016-11-20 16:06:38 -0500

    added models to admin pannel

--- advisingplus/admin.py
+++ advisingplus/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Advisor_Timeslot
 
 admin.site.register(Advisor_Timeslot)
-
+admin.site.register(Sessions)
+admin.site.register(notes)
+admin.site.register(Documents)
+admin.site.register(Student_Feedback)
