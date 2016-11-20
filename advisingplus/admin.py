5d8ce1d37c2993ac7202ecfa86620be56f21c09c
blaru001 <blaru001@fiu.edu>
2016-11-20 17:49:56 -0500

    corrected formats of models for admin view

--- advisingplus/admin.py
+++ advisingplus/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Advisor_Timeslot
+from .models import Advisor_Timeslot, Session, Note, Document, Student_Feedback 
 
 admin.site.register(Advisor_Timeslot)
-admin.site.register(Sessions)
-admin.site.register(notes)
-admin.site.register(Documents)
+admin.site.register(Session)
+admin.site.register(Note)
+admin.site.register(Document)
 admin.site.register(Student_Feedback)
--- advisingplus/models.py
+++ advisingplus/models.py
@@ -7,26 +7,24 @@ class Advisor_Timeslot(models.Model):
 
 
 #session created when a student is assigned to an Advisor_Timeslot
+
 class Session(models.Model):
-    timeslot = models.ForeignKey(Advisor_Timeslot, on_delete=models.CASCADE)
-    student = models.CharField(max_length=250)
+    timeslot = models.ForeignKey(Advisor_Timeslot,blank=True, on_delete=models.CASCADE)
+    student = models.CharField(max_length=250,blank=True)
     
-    def __str__(self):
-        return self.student
 
-
-class Notes(models.Model):
-    sessionId = models.ForeignKey(Session, on_delete=models.CASCADE)
+class Note(models.Model):
+    sessionId = models.ForeignKey(Session,blank=True, on_delete=models.CASCADE)
     content = models.CharField(max_length=400, blank=True)
 
 
-class Documents(models.Model):
-    sessionId = models.ForeignKey(Session, on_delete=models.CASCADE)
+class Document(models.Model):
+    sessionId = models.ForeignKey(Session, blank=True,  on_delete=models.CASCADE)
     docLink = models.FilePathField
     docType = models.CharField(max_length=10)
     requestReason = models.CharField(max_length=400)
     
 class Student_Feedback(models.Model):   
-    sessionId = models.ForeignKey(Session, on_delete=models.CASCADE)
+    sessionId = models.ForeignKey(Session, blank=True, on_delete=models.CASCADE)
     student_feedback = models.CharField(max_length=500)
      
