f4e3ee3f2e9c17c09797c3d64cb0d41d4690e465
blaru001 <blaru001@fiu.edu>
2016-11-20 19:25:22 -0500

    added __add__ and __radd__ methods to models for printing and concat

--- advisingplus/models.py
+++ advisingplus/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from datetime import datetime
+
 
 
 class Advisor_Timeslot(models.Model):
     date_time = models.DateTimeField(blank=True)
     advisor = models.CharField(max_length=250) 
+    
+    def __str__(self):
+        return self.advisor  + ' - ' + self.date_time.strftime(" %d - %m - %y , %I : %M %p")
+    
+    def __add__(self,other):
+        return str(self) + other
 
+    def __radd__(self,other):
+        return other + str(self)
 
 #session created when a student is assigned to an Advisor_Timeslot
 
@@ -12,11 +22,27 @@ class Session(models.Model):
     timeslot = models.ForeignKey(Advisor_Timeslot,blank=True, on_delete=models.CASCADE)
     student = models.CharField(max_length=250,blank=True)
     
+    def __str__(self):
+        return self.student + ' - ' + self.timeslot 
+
+    def __add__(self,other):
+        return str(self) + other
+
+    def __radd__(self,other):
+        return other + str(self)
 
 class Note(models.Model):
     sessionId = models.ForeignKey(Session,blank=True, on_delete=models.CASCADE)
     content = models.CharField(max_length=400, blank=True)
 
+    def __str__(self):
+        return self.sessionId + ' - ' +  self.content 
+
+    def __add__(self,other):
+        return str(self) + other
+
+    def __radd__(self,other):
+        return other + str(self)
 
 class Document(models.Model):
     sessionId = models.ForeignKey(Session, blank=True,  on_delete=models.CASCADE)
@@ -24,7 +50,24 @@ class Document(models.Model):
     docType = models.CharField(max_length=10)
     requestReason = models.CharField(max_length=400)
     
+    def __str__(self):
+        return self.sessionId + ' - ' + self.requestReason 
+
+    def __add__(self,other):
+        return str(self) + other
+
+    def __radd__(self,other):
+        return other + str(self)
+
 class Student_Feedback(models.Model):   
     sessionId = models.ForeignKey(Session, blank=True, on_delete=models.CASCADE)
     student_feedback = models.CharField(max_length=500)
      
+    def __str__(self):
+        return str(self.sessionId) 
+
+    def __add__(self,other):
+        return str(self) + other
+
+    def __radd__(self,other):
+        return other + str(self)
