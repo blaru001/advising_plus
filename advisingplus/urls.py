d038dec44fbf86e47e38085e867c9b1f578ac154
blaru001 <blaru001@fiu.edu>
2016-11-21 02:52:53 -0500

    created view add session and view session concept

--- advisingplus/urls.py
+++ advisingplus/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [ 
     url(r'^$', views.index, name='index'),
+    
+    # /i
+    #
+    url(r'^(?P<Session>[0-9]+)/$', views.view_Session, name="view_Session"),
+
     ]
 
 
--- advisingplus/views.py
+++ advisingplus/views.py
@@ -1,5 +1,15 @@
 from django.http  import HttpResponse
-
+from .models import Session, Advisor_Timeslot  
 
 def index(request):
-    return HttpResponse("<h3> This is the request service</h3>")
+    all_Sessions = Session.objects.all()
+    
+    html = ''
+    for session in all_Sessions:
+        url = '/' + str(session.id) + '/'
+        html += '<a href=' + url+ '>' + str(session) + '</a><br>'
+
+    return HttpResponse(html)
+
+def view_Session(request, Session):
+    return HttpResponse('<h2> details for session:' + str(Session) + ' </h2>') 
